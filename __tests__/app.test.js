da9a445a873ee0d353ccefedb2a7edf22725bb9e
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-11-07 16:08:01 -0800

    first test runnning

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,63 +8,77 @@ const client = require('../lib/client');
 
 describe('app routes', () => {
   describe('routes', () => {
-  
-  
+    let token;
+
     beforeAll(async done => {
       execSync('npm run setup-db');
-  
+
       client.connect();
-  
+    
+      const signInData = await fakeRequest(app) 
+        .post('/auth/signup')
+        .send({
+          email: 'jon@user.com',
+          password: '1234'
+        });
+
+      token = signInData.body.token;
+
       return done();
     });
-  
+
     afterAll(done => {
       return client.end(done);
     });
 
-  test('returns penguins', async() => {
+    test.only('returns penguins', async() => {
 
-    const expectation = [
-      {
-        name: 'emperor',
-        number_of_feet: 2,
-        eats_fish: true,
-        size: 'large',
-      },
-      {
-        name: 'king',
-        number_of_feet: 2,
-        eats_fish: true,
-        size: 'large',
-      },
-      {
-        name: 'african',
-        number_of_feet: 2,
-        eats_fish: true,
-        size: 'medium',
-      },
-      {
-        name: 'galapagos',
-        number_of_feet: 2,
-        eats_fish: true,
-        size: 'small',
-      },
-      {
-        name: 'gentoo',
-        number_of_feet: 2,
-        eats_fish: false,
-        size: 'small',
-      }
-    
-    ];
-    
+      const expectation = [
+        {
+          id: 1,
+          name: 'Emperor',
+          number_of_feet: 2,
+          eats_fish: true,
+          sizes: 1
+        },
+        {
+          id: 2,
+          name: 'King',
+          number_of_feet: 2,
+          eats_fish: true,
+          sizes: 2
+        },
+        {
+          id: 3,
+          name: 'African',
+          number_of_feet: 2,
+          eats_fish: true,
+          sizes: 3
+        },
+        {
+          id: 4,
+          name: 'Galapagos',
+          number_of_feet: 2,
+          eats_fish: true,
+          sizes: 2
+        },
+        {
+          id: 5,
+          name: 'Gentoo',
+          number_of_feet: 2,
+          eats_fish: false,
+          sizes: 3
+        }
+      ];
 
-    const data = await fakeRequest(app)
-      .get('/penguins')
-      .expect('Content-Type', /json/)
-      .expect(200);
+      const data = await fakeRequest(app)
+        .get('/penguins')
+        .expect('Content-Type', /json/)
+        .expect(200);
 
-    expect(data.body).toEqual(expectation);
+      expect(data.body).toEqual(expectation);
+    });
+
   });
 });
-,
+
